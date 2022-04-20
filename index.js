a18c20fd9cd21211b1e9b05fc138e794298c1961
Tom Cusack <Tom.Cusack@softwire.com>
2022-04-20 17:39:49 +0100

    Stretch 2 Added functionality for user to specify (in command-line arguments) either to ignore a rule or to add new rules.

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
+let FIZZ_ENABLED = true;
+let BUZZ_ENABLED = true;
+let BANG_ENABLED = true;
+let BONG_ENABLED = true;
+let FEZZ_ENABLED = true;
+let REVERSE_ENABLED = true;
+let CUSTOM_DIVISIBLE_RULES = {};
+
 function fizz_buzz(number) {
     let result = [];
 
-    if (number % 3 === 0) {
+    if (FIZZ_ENABLED && number % 3 === 0) {
         result.push("Fizz");
     }
-    if (number % 13 === 0) {
+    if (FEZZ_ENABLED && number % 13 === 0) {
         result.push("Fezz");
     }
-    if (number % 5 === 0) {
+    if (BUZZ_ENABLED && number % 5 === 0) {
         result.push("Buzz");
     }
-    if (number % 7 === 0) {
+    if (BANG_ENABLED && number % 7 === 0) {
         result.push("Bang");
     }
-    if (number % 11 === 0) {
+
+    // Other rules
+    for (let divisible_num in CUSTOM_DIVISIBLE_RULES) {
+        if (number % divisible_num === 0) {
+            result.push(CUSTOM_DIVISIBLE_RULES[divisible_num]);
+        }
+    }
+
+    if (BONG_ENABLED && number % 11 === 0) {
         // Edge case: Fezz is the only one that persists if Bong.
-        if (number % 13 === 0) {
+        if (FEZZ_ENABLED && number % 13 === 0) {
             result = ["Fezz", "Bong"];
         } else {
             result = ["Bong"];
         }
     }
-    if (number % 17 === 0) {
+    if (REVERSE_ENABLED && number % 17 === 0) {
         result.reverse();
     }
 
@@ -43,13 +59,32 @@ function test_given() {
         255: "BuzzFizz"
     };
 
-    console.log("\n-----");
+    // Reset custom rules, temporarily
+    const rules = [FIZZ_ENABLED, BUZZ_ENABLED, BANG_ENABLED, BONG_ENABLED, FEZZ_ENABLED, REVERSE_ENABLED, CUSTOM_DIVISIBLE_RULES];
+    FIZZ_ENABLED = true;
+    BUZZ_ENABLED = true;
+    BANG_ENABLED = true;
+    BONG_ENABLED = true;
+    FEZZ_ENABLED = true;
+    REVERSE_ENABLED = true;
+    CUSTOM_DIVISIBLE_RULES = {};
+
+    console.log("-----");
     for (let test_number in tests) {
         const expected = tests[test_number];
         const actual = fizz_buzz(test_number);
         console.assert(expected === actual, "fizz_buzz(" + test_number + "), expected " + expected + ", got " + actual);
     }
     console.log("All tests complete.\n-----");
+
+    // Reset custom rules to what they were
+    FIZZ_ENABLED = rules[0];
+    BUZZ_ENABLED = rules[1];
+    BANG_ENABLED = rules[2];
+    BONG_ENABLED = rules[3];
+    FEZZ_ENABLED = rules[4];
+    REVERSE_ENABLED = rules[5];
+    CUSTOM_DIVISIBLE_RULES = rules[6];
 }
 
 const readline = require("readline").createInterface({
@@ -57,10 +92,51 @@ const readline = require("readline").createInterface({
     output: process.stdout
 });
 
+// Deal with command line arguments
+let invalid_argument = false;
+for (let counter = 2; counter < process.argv.length; counter++) {
+    switch (process.argv[counter]) {
+        case "--no-fizz":
+            FIZZ_ENABLED = false;
+            break;
+        case "--no-buzz":
+            BUZZ_ENABLED = false;
+            break;
+        case "--no-bang":
+            BANG_ENABLED = false;
+            break;
+        case "--no-bong":
+            BONG_ENABLED = false;
+            break;
+        case "--no-fezz":
+            FEZZ_ENABLED = false;
+            break;
+        case "--help":
+            invalid_argument = true;
+            break;
+        default:
+            if (process.argv[counter].startsWith("--") && process.argv.length > counter + 1) {
+                let divisible = parseInt(process.argv[counter + 1], 10);
+                if (divisible > 0) { // filters out NaNs too
+                    CUSTOM_DIVISIBLE_RULES[divisible] = process.argv[counter].slice(2);
+                    counter++;
+                } else {
+                    invalid_argument = true;
+                }
+            } else {
+                invalid_argument = true;
+            }
+    }
+}
+if (invalid_argument) {
+    console.log("Invalid argument detected. Ignoring.");
+    console.log("Usage: node index.js [--help] [--no-[fizz|buzz|bang|bong|reverse]] [--NAME DIVISIBLE_NUMBER]");
+}
+
 // Main program
 readline.question("Enter a maximum number: ", user_maximum => {
     const maximum = parseInt(user_maximum, 10);
-    if (maximum > 0) { // gets rid of NaNs too
+    if (maximum > 0) { // filters out NaNs too
         for (let counter = 1; counter <= maximum; counter++) {
             console.log(fizz_buzz(counter));
         }
