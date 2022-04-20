0f32b08fa6b5449ff83eeb63ace61804c1fa10ad
Tom Cusack <Tom.Cusack@softwire.com>
2022-04-20 14:59:49 +0100

    Part 1 Attempt 1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// testing SourceTree commits
+function fizz_buzz(number) {
+    let result = "";
+    if (number % 3 === 0) {
+        result += "Fizz"
+    }
+    if (number % 5 === 0) {
+        result += "Buzz";
+    }
+    if (result === "") {
+        result = number.toString();
+    }
+    return result;
+}
+
+for (let counter = 1; counter <= 100; counter++) {
+    console.log(fizz_buzz(counter));
+}
