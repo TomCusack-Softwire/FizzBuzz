a3728c5a440ac2b80ac8c8de31193671c1e30542
Tom Cusack <Tom.Cusack@softwire.com>
2022-04-20 16:16:54 +0100

    Implemented Part 2 Changed to lists, so reversing was easier. Added automated testing (so I didn't have to scroll)

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 function fizz_buzz(number) {
-    let result = "";
+    let result = [];
+
     if (number % 3 === 0) {
-        result += "Fizz"
+        result.push("Fizz");
+    }
+    if (number % 13 === 0) {
+        result.push("Fezz");
     }
     if (number % 5 === 0) {
-        result += "Buzz";
+        result.push("Buzz");
     }
-    if (result === "") {
-        result = number.toString();
+    if (number % 7 === 0) {
+        result.push("Bang");
     }
-    return result;
+    if (number % 11 === 0) {
+        // Edge case: Fezz is the only one that persists if Bong.
+        if (number % 13 === 0) {
+            result = ["Fezz", "Bong"];
+        } else {
+            result = ["Bong"];
+        }
+    }
+    if (number % 17 === 0) {
+        result.reverse();
+    }
+
+    if (result.length === 0) {
+        return number.toString();
+    }
+
+    return result.join("");
 }
 
-for (let counter = 1; counter <= 100; counter++) {
+function test_given() {
+    // Ensuring that given tests are correct
+    let tests = {
+        21: "FizzBang",
+        33: "Bong",
+        65: "FezzBuzz",
+        195: "FizzFezzBuzz",
+        143: "FezzBong",
+        255: "BuzzFizz"
+    };
+
+    console.log("\n-----");
+    for (let test_number in tests) {
+        let expected = tests[test_number];
+        let actual = fizz_buzz(test_number);
+        console.assert(expected === actual, "fizz_buzz(" + test_number + "), expected " + expected + ", got " + actual);
+    }
+    console.log("All tests complete.\n-----");
+}
+
+// Main program
+for (let counter = 1; counter <= 300; counter++) {
     console.log(fizz_buzz(counter));
 }
+
+test_given();
